486a8de1f4dfa009c0e104d112dfd901654e8bc3
Memoryicecube <106273669+Memoryicecube@users.noreply.github.com>
2023-08-01 13:47:18 -0400

    environment build

---
+++ gulimall-coupon/src/test/java/com/atguigu/member/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.member.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-products/src/test/java/com/atguigu/gulimall/products/GulimallProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
